126eb8f95663bc2d710cbcaf9fe915f5fee43ae1
dev-tarun <tarun.yadav42009@gmail.com>
2026-06-11 17:10:28 +0530

    first commit

---
+++ overriding.java
@@ -0,0 +1,26 @@
+public class overriding { 
+     public static void  test(){
+            System.out.println("This is just a Perfect Human");
+        }
+    public static void main(String[] args) {
+      overriding.test();
+    }
+}
+
+ class overridingnext{
+      public  static void  test(){
+            System.out.println("This is just a normal test");
+        }
+        public static void main(String[] args) {
+            overridingnext.test();
+        }
+}
+
+class overout{
+     public  static void  test(){
+            System.out.println("This is just a normal Investment");
+        }
+        public static void main(String[] args) {
+            overout.test();
+        }
+}
