8ba913585214c2a1ebd8bdfdb26e01bee66df67b
dev-tarun <tarun.yadav42009@gmail.com>
2026-07-31 17:18:31 +0530

    :wq

--- overload.java
+++ overload.java
@@ -1,15 +1,18 @@
 public class overload {
-    public void put(){
+    public void put() {
         System.out.println("NO Parameter!");
     }
-    public void put(int x){
-        System.out.println("There is one parameter:- "+x);
+
+    public void put(int x) {
+        System.out.println("There is one parameter:- " + x);
     }
-    public void put(int x,int y){
-        System.out.println("There is two parameters:-"+x+" "+y);
+
+    public void put(int x, int y) {
+        System.out.println("There is two parameters:-" + x + " " + y);
     }
+
     public static void main(String[] args) {
-        overload o1=  new overload();
+        overload o1 = new overload();
         o1.put();
         o1.put(123);
         o1.put(12, 123);
--- overriding.java
+++ overriding.java
@@ -1,26 +1,29 @@
-public class overriding { 
-     public static void  test(){
-            System.out.println("This is just a Perfect Human");
-        }
+public class overriding {
+    public static void test() {
+        System.out.println("This is just a Perfect Human");
+    }
+
     public static void main(String[] args) {
-      overriding.test();
+        overriding.test();
     }
 }
 
- class overridingnext{
-      public  static void  test(){
-            System.out.println("This is just a normal test");
-        }
-        public static void main(String[] args) {
-            overridingnext.test();
-        }
+class overridingnext {
+    public static void test() {
+        System.out.println("This is just a normal test");
+    }
+
+    public static void main(String[] args) {
+        overridingnext.test();
+    }
 }
 
-class overout{
-     public  static void  test(){
-            System.out.println("This is just a normal Investment");
-        }
-        public static void main(String[] args) {
-            overout.test();
-        }
+class overout {
+    public static void test() {
+        System.out.println("This is just a normal Investment");
+    }
+
+    public static void main(String[] args) {
+        overout.test();
+    }
 }
