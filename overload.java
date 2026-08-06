98993921013707a7924b62dcf27a51c7ddb03aa5
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-06 17:08:21 +0530

    :wq

--- overload.java
+++ overload.java
@@ -1,20 +1,47 @@
+/**
+ * Demonstrates compile-time polymorphism through method overloading.
+ */
 public class overload {
+
+    /**
+     * Prints a default message when no arguments are provided.
+     */
     public void put() {
-        System.out.println("NO Parameter!");
+        System.out.println("No parameters provided.");
     }
 
+    /**
+     * Prints a message displaying the single integer argument.
+     * 
+     * @param x The first integer value
+     */
     public void put(int x) {
-        System.out.println("There is one parameter:- " + x);
+        System.out.printf("One parameter provided: %d%n", x);
     }
 
+    /**
+     * Prints a message displaying both integer arguments.
+     * 
+     * @param x The first integer value
+     * @param y The second integer value
+     */
     public void put(int x, int y) {
-        System.out.println("There is two parameters:-" + x + " " + y);
+        System.out.printf("Two parameters provided: %d and %d%n", x, y);
     }
 
+    /**
+     * Application entry point.
+     */
     public static void main(String[] args) {
-        overload o1 = new overload();
-        o1.put();
-        o1.put(123);
-        o1.put(12, 123);
+        overload overloadedObject = new overload();
+        
+        // Triggers the no-argument method
+        overloadedObject.put(); 
+        
+        // Triggers the single-argument method
+        overloadedObject.put(123); 
+        
+        // Triggers the two-argument method
+        overloadedObject.put(12, 123); 
     }
 }
